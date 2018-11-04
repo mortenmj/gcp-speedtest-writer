ce0734e7a716ab1639ebb5978c66c004343e61bb
Morten Mjelva <morten.mjelva@gmail.com>
2018-11-04 15:01:28 +0100

    Added application to source control

---
+++ app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const speedtest = require('./src/speedtest');
+
+exports.processSpeedtestEvent = async (pubSubMessage) => {
+  if (pubSubMessage.data) {
+      const speedtestEvent = pubSubMessage.data ? Buffer.from(pubSubMessage.data, 'base64').toString() : null;
+      await speedtest.handleSpeedtestEvent(JSON.parse(speedtestEvent));
+  }
+};
